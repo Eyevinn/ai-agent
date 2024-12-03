28dbd96216815d55c539d7753a2fbc19b6f2b4ac
ernestocarocca <113439130+ernestocarocca@users.noreply.github.com>
2024-12-03 13:18:12 +0100

    chore: update dependencies and add OpenAI chat endPoint

--- src/api.ts
+++ src/api.ts
@@ -7,6 +7,10 @@ import { Static, Type } from '@sinclair/typebox';
 import { FastifyPluginCallback } from 'fastify';
 import apiService from './api_service';
 
+import { OpenAI } from 'openai';
+import dotenv from 'dotenv';
+dotenv.config();
+
 const HelloWorld = Type.String({
   description: 'The magical words!'
 });
@@ -36,7 +40,42 @@ const healthcheck: FastifyPluginCallback<HealthcheckOptions> = (
   );
   next();
 };
+const aiMassage: FastifyPluginCallback = (fastify, opts, next) => {
+  async function chat(reply: any, userMessage: string) {
+    const openai = process.env.OPENAI_API_KEY as string;
+    if (!openai) {
+      throw new Error(
+        'The OPENAI_API_KEY environment variable is missing or empty; either provide it, or instantiate the OpenAI client with an apiKey option, like new OpenAI({ apiKey: "My API Key" }).'
+      );
+    }
+    const client = new OpenAI({ apiKey: openai });
+    reply.raw.setHeader('Content-Type', 'text/event-stream');
+    reply.raw.setHeader('Cache-Control', 'no-cache');
+    reply.raw.setHeader('Connection', 'keep-alive');
 
+    const chatCompletion = await client.chat.completions.create({
+      model: 'gpt-4',
+      messages: [
+        { role: 'system', content: 'You are a helpful assistant.' },
+        {
+          role: 'assistant',
+          content: [{ type: 'text', text: "Who's there?" }]
+        },
+        { role: 'user', content: userMessage }
+      ]
+    });
+
+    return reply.send(chatCompletion.choices[0].message.content);
+  }
+  fastify.get('/chat', async (_, reply) => {
+    await chat(reply, 'Hello');
+  });
+  fastify.post('/chat/send', async (request, reply) => {
+    const userMessage = request.body as string;
+    await chat(reply, userMessage);
+  });
+  next();
+};
 export interface ApiOptions {
   title: string;
 }
@@ -65,6 +104,7 @@ export default (opts: ApiOptions) => {
 
   api.register(healthcheck, { prefix: '/api', title: opts.title });
   // register other API routes here
+  api.register(aiMassage);
 
   if (!process.env.OPENAI_API_KEY) {
     throw new Error('OPENAI_API_KEY is required');
